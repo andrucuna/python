620a802bd94e1ae9ada01af105da4b963ba9ec40
Andrés Ruiz <andrucuna@Andress-MacBook-Pro.local>
2014-09-24 16:23:48 -0500

    MiniProject 1 - Rock Paper Scissors Lizard Spock

---
+++ interactivepython-coursera/interactivepython-assignment1/src/RPSLS.py
@@ -0,0 +1,88 @@
+__author__ = 'andrucuna'
+
+import random
+# Rock-paper-scissors-lizard-Spock template
+
+
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+# helper functions
+
+def name_to_number(name):
+    # convert name to number using if/elif/else
+    if name == "rock":
+        return 0
+    elif name == "Spock":
+        return 1
+    elif name == "paper":
+        return 2
+    elif name == "lizard":
+        return 3
+    elif name == "scissors":
+        return 4
+    else:
+        print "You've chosen a non-existent option"
+        return None
+    # don't forget to return the result!
+
+
+def number_to_name(number):
+    # convert number to a name using if/elif/else
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "Spock"
+    elif number == 2:
+        return "paper"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "scissors"
+    else:
+        print "You've chosen a non-existent option"
+        return None
+    # don't forget to return the result!
+
+
+def rpsls(player_choice):
+    # print a blank line to separate consecutive games
+    print ""
+    # print out the message for the player's choice
+    print "Player chooses " + player_choice
+    # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(5)
+    # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
+    # print out the message for computer's choice
+    print "Computer chooses " + comp_choice
+    # compute difference of comp_number and player_number modulo five
+    game_result = (comp_number - player_number) % 5
+    # use if/elif/else to determine winner, print winner message
+    if game_result == 0:
+        print "Player and computer tie!"
+    elif (game_result == 1) or (game_result == 2):
+        print "Computer wins!"
+    else:
+        print "Player wins!"
+
+
+# test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
+
+# always remember to check your completed program against the grading rubric
+
+
