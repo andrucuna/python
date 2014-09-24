0287944a7b0f096f71a9c98f326ad9fa26d35961
Andrés Ruiz <andrucuna@Andress-MacBook-Pro.local>
2014-09-24 11:53:31 -0500

    More basics on python - functions, conditionals

---
+++ interactivepython-coursera/interactivepython/week1/Conditionals.py
@@ -0,0 +1,27 @@
+__author__ = 'andrucuna'
+
+def greet(friend, money):
+    if friend and (money > 20):
+        print "Hi!"
+        money = money - 20
+    elif friend:
+        print "Hello"
+    else:
+        print "Ha ha"
+        money = money + 10
+    return money
+
+
+money = 15
+
+money = greet(True, money)
+print "Money:", money
+print ""
+
+money = greet(False, money)
+print "Money:", money
+print ""
+
+money = greet(True, money)
+print "Money:", money
+print ""
