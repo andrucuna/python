b03c94913da8987042f208d1dc33a7fd10dd3fa8
Andrés Ruiz <andrucuna@Andress-MacBook-Pro.local>
2014-10-08 11:07:11 -0500

    Examples of simple event-driven programs

---
+++ interactivepython-coursera/interactivepython/week2/EventDrivenProgr.py
@@ -0,0 +1,16 @@
+__author__ = 'andrucuna'
+
+# Example of a simple event-driven program
+
+# CodeSkulptor GUI module
+import simplegui
+
+# Event handler
+def tick():
+    print "tick!"
+
+# Register handler
+timer = simplegui.create_timer(1000, tick)
+
+# Start timer
+timer.start()
